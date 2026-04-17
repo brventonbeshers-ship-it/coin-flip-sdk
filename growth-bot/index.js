27888f7b1e65f56575abe24304a6bf7f9db79b97
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-17 13:18:33 +0300

    chore: clean up index.js

--- growth-bot/index.js
+++
@@ -1,5 +0,0 @@
-import * as sdk from 'coin-flip-sdk';
-
-console.log('Growth bot running for coin-flip-sdk...');
-console.log('SDK exports:', Object.keys(sdk).join(', '));
-console.log('Smoke check passed.');
